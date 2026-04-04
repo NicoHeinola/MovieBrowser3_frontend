47706cee4681d914767ef0dfcc7fcce4c8c47f12
Nico Heinola <nico.heinola@gmail.com>
2026-04-05 00:31:59 +0300

    feat: add TypeScript ESLint plugin and update configuration rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,4 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
 import prettier from 'eslint-config-prettier';
 import vuetify from 'eslint-config-vuetify';
 
@@ -5,6 +6,9 @@ export default [
   ...(await vuetify()),
   prettier,
   {
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
     rules: {
       'antfu/top-level-function': 'off',
       '@typescript-eslint/no-explicit-any': 'error',
