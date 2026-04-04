d61bb29df564818f47b1b5b60be043632711e669
Nico Heinola <nico.heinola@gmail.com>
2026-04-04 23:10:45 +0300

    refactor: clean up ESLint config and improve main.ts import statements

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,5 @@
 import vuetify from 'eslint-config-vuetify'
-import { SemicolonPreference } from "typescript/lib/typescript"
 
 export default vuetify({
   ts: true,
-  SemicolonPreference: SemicolonPreference.Insert,
 })
