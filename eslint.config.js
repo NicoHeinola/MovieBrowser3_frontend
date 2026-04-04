20f6e8789433ceaf32c4297067713a22c675976f
Nico Heinola <nico.heinola@gmail.com>
2026-04-04 23:20:54 +0300

    refactor: update code style and structure across multiple files

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,10 @@
-import vuetify from 'eslint-config-vuetify'
+import vuetify from 'eslint-config-vuetify';
 
 export default vuetify({
-  ts: true,
-})
+  rules: {
+    'quotes': ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+    'semi': ['error', 'always'],
+    'no-extra-semi': 'error',
+    '@stylistic/semi': ['error', 'always'],
+  },
+});
--- src/router/index.ts
+++ src/router/index.ts
@@ -5,8 +5,8 @@
  */
 
 // Composables
-import { createRouter, createWebHistory } from 'vue-router'
-import Index from '@/pages/index.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Index from '@/pages/index.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +16,6 @@ const router = createRouter({
       component: Index,
     },
   ],
-})
+});
 
-export default router
+export default router;
