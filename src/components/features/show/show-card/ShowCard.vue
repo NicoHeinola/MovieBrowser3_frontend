0add8e2a89d0259e2e7000931eb9214430a71b42
Nico Heinola <nico.heinola@gmail.com>
2026-04-05 04:24:27 +0300

    feat: update ShowCard component styling and refactor Home page to enhance show display logic

--- src/components/features/show/show-card/ShowCard.vue
+++ src/components/features/show/show-card/ShowCard.vue
@@ -6,7 +6,7 @@ const props = defineProps<{
 </script>
 
 <template>
-  <v-card :class="{ 'border-sm': props.isSelected }" class="pa-0 bg-white">
+  <v-card :style="{ outline: props.isSelected ? '1px solid white' : '' }" class="pa-0 bg-white">
     <v-img :src="props.imageUrl" class="h-100 w-100" cover></v-img>
   </v-card>
 </template>
--- src/pages/public/Home.vue
+++ src/pages/public/Home.vue
@@ -1,9 +1,10 @@
 <script lang="ts" setup>
 import type { Show } from '@/interfaces/api/Show';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import ShowCard from '@/components/features/show/show-card/ShowCard.vue';
+import { useDisplay } from 'vuetify';
 
-const shows = ref<Show[]>([
+const continueToWatchShows = ref<Show[]>([
   {
     id: 1,
     hero_image_url: 'https://images6.alphacoders.com/128/1280527.jpg',
@@ -49,33 +50,42 @@ const shows = ref<Show[]>([
     ],
   },
 ]);
+
+const selectedBannerShow = ref<Show | null>(null);
+
+const { xxl, xlAndUp, lgAndUp } = useDisplay();
+
+const bannerCardContainerWidth = computed(() => {
+  const basePadding = 40;
+  const baseCardSize = 300;
+
+  if (xxl.value) return (basePadding + baseCardSize) * 5 - 30 + 'px';
+  if (xlAndUp.value) return (basePadding + baseCardSize) * 4 - 30 + 'px';
+  if (lgAndUp.value) return (basePadding + baseCardSize) * 3 - 30 + 'px';
+  return '310px';
+});
 </script>
 
 <template>
   <div class="position-absolute w-100" style="left: 50%; transform: translateX(-50%); top: 0">
     <div style="height: 70vh">
-      <v-img
-        class="banner-image"
-        height="100%"
-        src="https://m.media-amazon.com/images/M/MV5BNTQzNDI5OGItZDZkMy00MWQ1LWIwM2YtYzc2YWNhOGJlZWQxXkEyXkFqcGc@._V1_.jpg"
-        cover
-      >
-      </v-img>
+      <v-img class="banner-image" height="100%" :src="selectedBannerShow?.hero_image_url" cover> </v-img>
       <div class="position-absolute w-100 h-50 bottom-0 banner-gradient-background"></div>
     </div>
     <div class="position-absolute w-100 h-100 d-flex flex-column align-center justify-center top-0">
-      <div style="max-width: 980px">
+      <div :style="{ maxWidth: `min(${bannerCardContainerWidth}, 90%)` }">
         <h1>Continue Watching</h1>
-        <div class="d-flex align-center ga-10 w-100 overflow-scroll">
+        <div class="d-flex align-center ga-10 w-100 pa-1 overflow-scroll">
           <show-card
-            v-for="show in [...shows]"
+            v-for="show in [...continueToWatchShows]"
             :image-url="show.card_image_url"
             :show="show"
             height="400px"
             min-width="300px"
             width="300px"
-            is-selected
+            :is-selected="selectedBannerShow?.id === show.id"
             :key="show.id"
+            @mouseenter="selectedBannerShow = show"
           />
         </div>
       </div>
