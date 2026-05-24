157d9c0a7f4ab9d9128bd7bc49577011a9b8e811
Nico Heinola <nico.heinola@gmail.com>
2026-05-24 12:15:00 +0300

    feat(show-edit-dialog): add links tab and related components

--- src/components/features/show/show-edit-dialog/ShowEditDialog.vue
+++ src/components/features/show/show-edit-dialog/ShowEditDialog.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
 import type { DialogComponentProps } from '@/components/layouts/dialog-provider';
 import type { Show } from '@/interfaces/api/models/Show';
+
 import { ref } from 'vue';
+
 import { BaseDialog } from '@/components/common/base-dialog';
 import BaseForm from '@/components/common/base-form/BaseForm.vue';
 import ShowEntriesTab from './ShowEntriesTab.vue';
 import ShowGeneralTab from './ShowGeneralTab.vue';
+import ShowLinksTab from './ShowLinksTab.vue';
 import ShowUiTab from './ShowUiTab.vue';
 
-const props = defineProps<DialogComponentProps>();
+const props = defineProps<DialogComponentProps<Show>>();
 
 const show = defineModel<Show>('show', { required: true });
 const isFormValid = ref<boolean>(false);
@@ -20,7 +23,7 @@ const selectedTab = ref<string[]>(['general']);
 <template>
   <base-dialog v-model="isDialogVisible" :max-width="1060">
     <template #title>
-      {{ show ? 'Edit Show' : 'Create Show' }}
+      {{ show?.id ? 'Edit Show' : 'Create Show' }}
     </template>
 
     <template #default>
@@ -44,17 +47,7 @@ const selectedTab = ref<string[]>(['general']);
               <show-entries-tab v-model:show="show" />
             </v-tabs-window-item>
             <v-tabs-window-item class="pa-1" value="links">
-              <v-sheet class="pa-5" color="brown">
-                This page should display links related to the show. We have incoming links that this show is part of.
-                And outgoing links that this show points to. If you, for example make this show a prequel to another
-                show, then that show would be an outgoing link from this show, and this show would be an incoming link
-                to the other show. This would also has "magic" logic that adds to the show that this show itself is a
-                sequel of the other show, so that you can easily find all shows that are prequels or sequels of a show.
-                You can also add custom links that are not part of the "magic" logic, for example if you want to link
-                two shows together that are not prequels or sequels of each other, but still have some relation to each
-                other. This magic logic works for "suggested_next" and "suggested_previous" links, but not for "related"
-                links, as those are just custom links that have no special logic.
-              </v-sheet>
+              <show-links-tab v-model:show="show" />
             </v-tabs-window-item>
           </v-tabs-window>
         </base-form>
@@ -64,7 +57,7 @@ const selectedTab = ref<string[]>(['general']);
     <template #actions>
       <v-spacer />
       <v-btn variant="text" @click="props.close()"> Cancel </v-btn>
-      <v-btn color="primary" variant="flat" @click="props.close()"> Save </v-btn>
+      <v-btn color="primary" variant="flat" @click="props.close(show)"> Save </v-btn>
     </template>
   </base-dialog>
 </template>
---
+++ src/components/features/show/show-edit-dialog/ShowLinksTab.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import type { Show } from '@/interfaces/api/models/Show';
+
+import ShowLinksFormPart from './ShowLinksFormPart.vue';
+
+const show = defineModel<Show>('show', { required: true });
+</script>
+
+<template>
+  <show-links-form-part v-model:show="show" />
+</template>
