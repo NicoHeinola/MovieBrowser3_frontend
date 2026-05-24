cee17cfa4113c8c4af0945c5da0572ef3954606c
Nico Heinola <nico.heinola@gmail.com>
2026-05-24 11:52:53 +0300

    feat(show-edit-dialog): add detailed description for links tab functionality

--- src/components/features/show/show-edit-dialog/ShowEditDialog.vue
+++ src/components/features/show/show-edit-dialog/ShowEditDialog.vue
@@ -44,7 +44,17 @@ const selectedTab = ref<string[]>(['general']);
               <show-entries-tab v-model:show="show" />
             </v-tabs-window-item>
             <v-tabs-window-item class="pa-1" value="links">
-              <v-sheet class="pa-5" color="brown">Three</v-sheet>
+              <v-sheet class="pa-5" color="brown">
+                This page should display links related to the show. We have incoming links that this show is part of.
+                And outgoing links that this show points to. If you, for example make this show a prequel to another
+                show, then that show would be an outgoing link from this show, and this show would be an incoming link
+                to the other show. This would also has "magic" logic that adds to the show that this show itself is a
+                sequel of the other show, so that you can easily find all shows that are prequels or sequels of a show.
+                You can also add custom links that are not part of the "magic" logic, for example if you want to link
+                two shows together that are not prequels or sequels of each other, but still have some relation to each
+                other. This magic logic works for "suggested_next" and "suggested_previous" links, but not for "related"
+                links, as those are just custom links that have no special logic.
+              </v-sheet>
             </v-tabs-window-item>
           </v-tabs-window>
         </base-form>
