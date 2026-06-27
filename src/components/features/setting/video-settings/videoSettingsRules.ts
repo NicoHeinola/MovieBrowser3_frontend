867059f044f6ea9214c0216e9ca02ea5aa5e5326
Nico Heinola <nico.heinola@gmail.com>
2026-06-27 14:53:09 +0300

    refactor(video-settings): remove required rule from validation for paths

--- src/components/features/setting/video-settings/videoSettingsRules.ts
+++ src/components/features/setting/video-settings/videoSettingsRules.ts
@@ -1,9 +1,8 @@
 import { filePathRule } from '@/rules/filePathRule';
-import { requiredRule } from '@/rules/requiredRule';
 
 export const getRules = () => {
   return {
-    videoBasePath: [(value: string) => requiredRule(value.trim()), (value: string) => filePathRule(value.trim())],
-    vlcMediaPlayerPath: [(value: string) => requiredRule(value.trim()), (value: string) => filePathRule(value.trim())],
+    videoBasePath: [(value: string) => filePathRule(value.trim())],
+    vlcMediaPlayerPath: [(value: string) => filePathRule(value.trim())],
   };
 };
